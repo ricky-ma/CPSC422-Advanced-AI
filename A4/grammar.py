88fe03f78b45b971c83e7921aac60e77adb596b8
ricky-ma <mr.rickyma@gmail.com>
2021-04-02 22:39:33 -0700

    Matching for second-level phrases

--- A4/grammar.py
+++ A4/grammar.py
@@ -38,18 +38,31 @@ phrases = [x for x in phrases if
            '\n' not in x and
            x[0] == '(' and
            x[-1] == ')' and
-           x.count('(') % 2 == 1]
-# print(phrases)
+           x.count('(') % 2 == 1 and
+           (x[3] == '(' or x[4] == '(' or x[5] == '(' or x.count('(') == 1)]
+print(phrases)
 print(len(phrases))
 
 counts = {}
 for phrase in phrases:
+    # Get main part of speech from phrase
+    part_of_speech, *_ = re.findall('\((.*?) ', phrase)
+
     # Single word
     if phrase.count('(') == 1:
-        part_of_speech = re.search('\((.*) ', phrase).group(1)
         word = re.search('/(.*)/', phrase).group(1)
         try:
             counts[part_of_speech + '_' + word] += 1
         except KeyError as e:
             counts[part_of_speech + '_' + word] = 1
+
+    if phrase.count('(') == 3:
+        phrase = phrase[len(part_of_speech) + 2:]
+        pos1, *_ = re.findall('\((.*?) ', phrase)
+        phrase = phrase[len(part_of_speech) + 2:]
+        pos2, *_ = re.findall('\((.*?) ', phrase)
+        try:
+            counts[part_of_speech + '_' + pos1 + '_' + pos2] += 1
+        except KeyError as e:
+            counts[part_of_speech + '_' + pos1 + '_' + pos2] = 1
 print(counts)
